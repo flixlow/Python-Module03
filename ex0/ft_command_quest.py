8c46ac0481dc9a676ec46ebe25d22f6329d7ca34
flixlow <felix.lauwerie@gmail.com>
2026-01-26 22:42:36 +0100

    feat: ft_command_quest.py

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def ft_command_quest() -> None:
+    argc = len(sys.argv)
+
+    if argc < 2:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {argc - 1}")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print(f"Argument {i}: {arg}")
+
+    print(f"Program name: {sys.argv[0]}")
+    print(f"Total arguments: {argc}")
+
+
+if __name__ == "__main__":
+    ft_command_quest()
---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def ft_score_analytics():
+    sys
+
+
+if __name__ == "__main__":
+    ft_score_analytics()
