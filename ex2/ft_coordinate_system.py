3b31f9e14898afc7248ab16f66a9c269140553d0
flixlow <felix.lauwerie@gmail.com>
2026-01-31 12:56:39 +0100

    coordinate system

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,44 +1,55 @@
 #!/usr/bin/env python3
 
-import sys
 import math
 
 
-def distance(pos: tuple) -> None:
-    x, y, z = pos
-    ret = math.sqrt(x**2 + y**2 + z**2)
-    print(f"Distance between (0, 0, 0) and {pos}: {ret:.2f}")
+def distance(src: tuple = (0, 0, 0), dest: tuple = (0, 0, 0)) -> None:
+    x1, y1, z1 = src
+    x2, y2, z2 = dest
+    ret = math.sqrt((x1 - x2)**2 + (y1 - y2)**2 + (z1 - z2)**2)
+    print(f"Distance between {dest} and {src}: {ret:.2f}")
 
 
-def check_coordinates() -> tuple:
+def parsing_coordinates(unchecked_coordinates: str) -> tuple:
     try:
-        x = type(sys.argv[1])
-        y = type(sys.argv[2])
-        z = type(sys.argv[3])
-    except ValueError:
-        raise ValueError()
-    return (x, y, z)
+        coordinates_list = unchecked_coordinates.split(",")
+        x = int(coordinates_list[0])
+        y = int(coordinates_list[1])
+        z = int(coordinates_list[2])
+    except ValueError as e:
+        print(f"Error parsing coordinates: {e}")
+        print(f"Error details - Type: {type(e).__name__}, Args: {e}")
+    else:
+        return (x, y, z)
 
 
 def ft_coordinate_system():
-    try:
-        pos = check_coordinates()
-    except ValueError as e:
-        print(f"Error details - Type: ValueError, Args: ({e})")
+    print("=== Game Coordinate System ===")
+    print()
+    pos = parsing_coordinates("10,20,5")
     print(f"Position created: {pos}")
     distance(pos)
-
-
-def main():
-    print("=== Game Coordinate System ===")
-    ft_coordinate_system()
+    print()
+    coords = "3,4,0"
+    print(f"Parsing coordinates: \"{coords}\"")
+    pos = parsing_coordinates(coords)
+    print(f"Parsed position: {pos}")
+    distance(pos)
+    print()
+    coords = "abc,def,ghi"
+    print(f"Parsing coordinates: \"{coords}\"")
+    print()
+    (x, y, z) = pos
+    print("Unpacking demonstration:")
+    print(f"Player at x={x}, y={y}, z={z}")
+    print(f"Coordinates: X={x}, Y={y}, Z={z}")
 
 
 if __name__ == "__main__":
-    main()
+    ft_coordinate_system()
 
 # === Game Coordinate System ===
-# Position created: (10, 20, 5)
+# Position : (10, 20, 5)
 # Distance between (0, 0, 0) and (10, 20, 5): 22.91
 
 # Parsing coordinates: "3,4,0"
